1476a5f63accaaead521eecd24abc5d73c5f436a
Nikita <zelenb02@gmail.com>
2024-04-27 12:37:48 +0500

    first commit

---
+++ program.cpp
@@ -0,0 +1,11 @@
+#include "graphics.h"
+#include "picture.hpp"
+int main()
+{ initwindow(800,600);
+ house(); // дом
+ sun(); // солнце
+ man(); // человек
+ fence(); // забор
+ getch();
+ closegraph();
+}
